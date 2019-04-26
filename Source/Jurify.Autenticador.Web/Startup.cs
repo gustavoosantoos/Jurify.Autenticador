05fc0b41167b4c850e64796dd74281269414bab5
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-04-26 13:47:28 -0300

    Adição do projeto web de autenticação com base no padrão do IdentityServer4

---
+++ Source/Jurify.Autenticador.Web/Startup.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jurify.Autenticador.Web
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var builder = services.AddIdentityServer()
+                .AddTestUsers(TestUsers.Users)
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients());
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}

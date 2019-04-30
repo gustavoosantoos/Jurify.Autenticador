f79d043f21c849752c4a9d160e5f6ccb85d5c8d6
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-04-30 17:10:52 -0300

    Add OfficerInfo value object

---
+++ Source/Jurify.Autenticador.Domain/ValueObjects/OfficeInfo.cs
@@ -0,0 +1,20 @@
+﻿using Jurify.Autenticador.Domain.Base;
+using System.Collections.Generic;
+
+namespace Jurify.Autenticador.Domain.ValueObjects
+{
+    public class OfficeInfo : ValueObject
+    {
+        public string Name { get; private set; }
+
+        public OfficeInfo(string name)
+        {
+            Name = name;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Name;
+        }
+    }
+}

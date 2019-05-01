3d06da597eac730edc32758a8cbbf1a08664f8ff
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-05-01 19:05:30 -0300

    Adding SignUp properties to viewmodel

--- Source/Jurify.Autenticador.Web/Features/Account/SignUpViewModel.cs
+++ Source/Jurify.Autenticador.Web/Features/Account/SignUpViewModel.cs
@@ -2,5 +2,15 @@
 {
     public class SignUpViewModel
     {
+        public string OfficeName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }

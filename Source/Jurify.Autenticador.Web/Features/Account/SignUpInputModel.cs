a2bd2a272db67e70cef2740a5ee8d24a09212cba
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-05-01 22:32:44 -0300

    Updating SignUp InputModel and ViewModel

--- Source/Jurify.Autenticador.Web/Features/Account/SignUpInputModel.cs
+++ Source/Jurify.Autenticador.Web/Features/Account/SignUpInputModel.cs
@@ -2,5 +2,14 @@
 {
     public class SignUpInputModel
     {
+        public string OfficeName { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
--- Source/Jurify.Autenticador.Web/Features/Account/SignUpViewModel.cs
+++ Source/Jurify.Autenticador.Web/Features/Account/SignUpViewModel.cs
@@ -1,16 +1,7 @@
 ﻿namespace Jurify.Autenticador.Web.Features.Account
 {
-    public class SignUpViewModel
+    public class SignUpViewModel : SignUpInputModel
     {
-        public string OfficeName { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
         public string ReturnUrl { get; set; }
     }
 }

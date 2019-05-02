5541bd0c91771a0ec59eee953c5ff7fb2eb6370e
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-05-02 13:29:14 -0300

    Including folder to login for clients and lawyers

---
+++ Source/Jurify.Autenticador.Web/Features/AccountClients/AccountClientsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jurify.Autenticador.Web.Features.AccountClients
+{
+    public class AccountClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/Jurify.Autenticador.Web/Features/AccountLawyers/AccountLawyersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jurify.Autenticador.Web.Features.AccountLawyers
+{
+    public class AccountLawyersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

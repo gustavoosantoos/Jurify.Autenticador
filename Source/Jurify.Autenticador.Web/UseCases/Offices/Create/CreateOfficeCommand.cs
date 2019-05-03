612ed3f378f4ea9302496b125f2ee6d2966f1970
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-05-03 11:22:34 -0300

    Creating office command props and validation

--- Source/Jurify.Autenticador.Web/UseCases/Offices/Create/CreateOfficeCommand.cs
+++ Source/Jurify.Autenticador.Web/UseCases/Offices/Create/CreateOfficeCommand.cs
@@ -4,6 +4,8 @@ namespace Jurify.Autenticador.Web.UseCases.Offices.Create
 {
     public class CreateOfficeCommand : IRequest
     {
-
+        public string OfficeName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
--- Source/Jurify.Autenticador.Web/UseCases/Offices/Create/CreateOfficeCommandValidator.cs
+++ Source/Jurify.Autenticador.Web/UseCases/Offices/Create/CreateOfficeCommandValidator.cs
@@ -4,8 +4,25 @@ namespace Jurify.Autenticador.Web.UseCases.Offices.Create
 {
     public class CreateOfficeCommandValidator : AbstractValidator<CreateOfficeCommand>
     {
+        private const string OfficeNameEmptyErrorMessage = "O nome do escritório deve ser informado";
+        private const string OfficeNameLengthErrorMessage = "O nome do escritório deve ter ao máximo 100 caracteres";
+
+        private const string LongitudeRangeErrorMessage = "A longitude deve estar entre -180 e 180 graus";
+        private const string LatitudeRangeErrorMessage = "A latitude deve estar entre -90 e 90 graus";
+
         public CreateOfficeCommandValidator()
         {
+            RuleFor(e => e.OfficeName)
+                .NotEmpty().WithMessage(OfficeNameEmptyErrorMessage)
+                .Length(1, 100).WithMessage(OfficeNameLengthErrorMessage);
+
+            RuleFor(e => e.Latitude)
+                .GreaterThanOrEqualTo(-90).WithMessage(LatitudeRangeErrorMessage)
+                .LessThanOrEqualTo(90).WithMessage(LatitudeRangeErrorMessage);
+
+            RuleFor(e => e.Longitude)
+                .GreaterThanOrEqualTo(-180).WithMessage(LongitudeRangeErrorMessage)
+                .LessThanOrEqualTo(180).WithMessage(LongitudeRangeErrorMessage);
         }
     }
 }

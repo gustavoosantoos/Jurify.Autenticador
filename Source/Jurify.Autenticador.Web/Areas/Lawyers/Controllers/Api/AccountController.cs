27e4d06821c169c88056bff13352ed8ec98049ed
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-06-08 19:02:41 -0300

    feat(account): create office and user using api skeleton

---
+++ Source/Jurify.Autenticador.Web/Areas/Lawyers/Controllers/Api/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Jurify.Autenticador.Web.Infrastructure.SecurityHelpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jurify.Autenticador.Web.Areas.Lawyers.Controllers.Api
+{
+    [Area("Lawyers")]
+    [Route("api/[controller]")]
+    [EnableCors("Default")]
+    [ApiController]
+    [AllowAnonymous]
+    [SecurityHeaders]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("/OfficeAvailable/{officeName}")]
+        public ActionResult OfficeAvailable(string officeName)
+        {
+            return Ok();
+        }
+
+        [HttpGet("/UserAvailable/{username}")] 
+        public ActionResult UserAvailable(string username)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOffice(string officeName, string username, string plainPassword)
+        {
+            return Ok();
+        }
+    }
+}

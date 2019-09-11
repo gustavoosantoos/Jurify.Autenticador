43d097a4b7f6344fa3dcaaf79ae50160f8bf838d
dlqseronato <dlqseronato@gmail.com>
2019-09-11 13:23:24 -0300

    Inclusão da postagem na fila de mensageria para a APi PerfilOab ao criar um usuario inicial

---
+++ Source/Jurify.Autenticador.Web/UseCases/Advogados/CriarUsuarioInicial/Oab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jurify.Autenticador.Web.UseCases.Advogados.CriarUsuarioInicial
+{
+    public class Oab
+    {
+        public string NumeroOab { get; set; }
+        public string Uf { get; set; }
+        public string NomeCompleto { get; set; }
+
+        public Oab(string numeroOab, string uf, string nomeCompleto)
+        {
+            NumeroOab = numeroOab;
+            Uf = uf;
+            NomeCompleto = nomeCompleto;
+        }
+    }
+}
---
+++ Source/Jurify.Autenticador.Web/UseCases/Advogados/ValidarUsuarioInicial/OabHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jurify.Autenticador.Web.Infrastructure.Database.Context;
+
+namespace Jurify.Autenticador.Web.UseCases.Advogados.ValidaUsuarioInicial
+{
+    public class OabHandler
+    {
+        private readonly AutenticadorContext _context;
+
+        public OabHandler(AutenticadorContext context)
+        {
+            _context = context;
+        }
+        /* Resolver Lagomorpha 2.0 não é compatível com netcoreapp2.1
+        [QueueHandler("validar-oab")]
+        public void HandleNewOab(Oab oab)
+        {
+
+            _context.Oab.Add(oab);
+            _context.SaveChanges();
+            Debug.WriteLine(oab.NomeCompleto);
+            Console.WriteLine("=================== MENSAGEM RECEBIDA =================");
+        }
+        */
+    }
+}
